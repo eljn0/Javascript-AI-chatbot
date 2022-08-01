d6cf41d26cae64973e7832bb335a11ae8bb38506
Eljan Guluyev <77200703+eljn0@users.noreply.github.com>
2022-08-01 13:39:11 +0400

    Add files via upload

---
+++ turkish airlines/aibot.js
@@ -0,0 +1,19 @@
+function talk(){  
+    var know = {  
+     "Hi" : " Hi, How can I help you?",
+     "Who are you?" : "Hi, I am Turkish Airlines' chatbot. How can I help you? ",
+     "Who are you" : "Hi, I am Turkish Airlines' chatbot. How can I help you?",  
+     "How are you" : "I am feeling goog to help you.",   
+     "How are you?" : "I am feeling goog to help you.",  
+     "Hwy" : "I am feeling goog to help you.",   
+     "Bye" : "Okay! Will meet soon..",
+     "Weight restrictions": "In Economy class you can bring maximum 8kg bagage. "
+    }
+    var user = document.getElementById('userBox').value;  
+     document.getElementById('chatLog').innerHTML = user + "<br>";  
+    if (user in know) {  
+     document.getElementById('chatLog').innerHTML = know[user] + "<br>";  
+    }else{  
+     document.getElementById('chatLog').innerHTML = "Sorry,I didn't understand <br>";  
+    }  
+   } 
